27cf1a6c4a497c3625be3a3d489c7bb829a8ab4e
TopGitSan <c.toparcean@gmail.com>
2019-05-01 23:38:50 +0200

    format currency and change amount

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learnPipes';
+  title = 'learn Pipes';
+
+  private actualAmountInEUR = 101.25;
+
+  eur = true;
+  currency = 'USD';
+  rateUsdEur = 1.12;
+
+  get amount(){
+    return this.eur ? this.actualAmountInEUR : this.actualAmountInEUR*this.rateUsdEur
+  }
+
+  get format(){
+    return this.eur ? 'EUR' : 'USD';
+  }
+
+  toggleCurrency(){
+    this.eur = !this.eur;
+    this.currency = this.eur ? 'USD' : 'EUR';
+  }
+
 }
