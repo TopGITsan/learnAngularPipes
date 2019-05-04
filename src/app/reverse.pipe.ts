d98058b207aa0c002f8b7d9a2f43161f1a52133c
TopGitSan <c.toparcean@gmail.com>
2019-05-04 20:02:28 +0200

    add a basic custom pipi

--- src/app/reverse.pipe.ts
+++ src/app/reverse.pipe.ts
@@ -5,9 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReversePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    console.log(value);
-    return null;
+  transform(value: any, ...args): any {
+    console.log(value, args)
+    let newstring = value
+    if (args[0]){ // I want a full stop
+      newstring += "!"
+    }
+    if (args[1] === "singlequote"){
+      newstring = ": " + newstring;
+    } else if (args[2] === "doublequote"){
+      newstring = '; ' + newstring
+    }
+    return newstring;
   }
 
 }
